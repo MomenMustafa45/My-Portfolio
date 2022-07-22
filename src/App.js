74d58ec780e51b2a1aefc70969c571f40da81b59
MomenMustafa45 <momenkara2@gmail.com>
2022-07-22 23:46:47 +0200

    moved pages to app.js file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import "./App.css";
-import Pages from "./Components/Pages/Pages";
+// import Pages from "./Components/Pages/Pages";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Aos from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
+import Header from "./Components/Common/Header";
+import Home from "./Components/Home/Home";
+import About from "./Components/Pages/About";
+import Services from "./Components/Pages/Services";
+import Portfolio from "./Components/Pages/Portfolio";
+import Testimonials from "./Components/Pages/Testimonials";
+import Contact from "./Components/Pages/Contact";
+import Footer from "./Components/Common/Footer";
 
 function App() {
   useEffect(() => {
@@ -11,7 +20,18 @@ function App() {
   }, []);
   return (
     <div className="App">
-      <Pages />
+      <Router basename={process.env.PUBLIC_URL}>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/services" element={<Services />} />
+          <Route exact path="/portfolio" element={<Portfolio />} />
+          <Route exact path="/certification" element={<Testimonials />} />
+          <Route exact path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
